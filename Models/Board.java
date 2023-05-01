3bd6fceaf0adb33c1b9aa077808a1603ef881ab0
rajaram0077 <rajaram.korlepara@gmail.com>
2023-05-01 19:59:54 +0530

    titactoe initial commit

---
+++ Models/Board.java
@@ -0,0 +1,22 @@
+public class Board {
+
+   private Board board;
+
+   private List<Player> player;
+
+   private int nextPlayer id;
+
+
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public List<Player> getPlayer() {
+        return player;
+    }
+
+    public int getNextPlayer() {
+        return nextPlayer;
+    }
+}
